589ac93de789f1fe589ebb54f0def0b97a14b777
Milind Gour <milind.gour@nagarro.com>
2025-08-08 15:18:21 +0530

    add more formatters
    - formatters for jsp, svelte, and go added

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,11 +1,17 @@
 return {
 	"stevearc/conform.nvim",
-	event = "VeryLazy",
 	config = function()
 		require("conform").setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
 				javascript = { "prettierd", "prettier", stop_after_first = true },
+				go = { "goimports", "gofmt" },
+				svelte = { "prettier" },
+				jsp = { "html_beautify" },
+			},
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_format = "fallback",
 			},
 		})
 	end,
