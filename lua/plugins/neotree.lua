8142e4b4354a30b5c2494f15cec15bbf3fbd3efd
Milind Gour <milind.gour@nagarro.com>
2025-08-13 21:21:56 +0530

    changes

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -11,39 +11,36 @@ return {
 		keys = {
 			{ "<leader>e", "<cmd>Neotree toggle<CR>", { desc = "Toggle neo-tree" } },
 		},
-		config = function()
-			local neotree = require("neo-tree")
-			neotree.setup({
-				window = {
-					width = 25,
-					mappings = {
-						["l"] = "open",
-						["h"] = "close_node",
+		opts = {
+			window = {
+				width = 25,
+				mappings = {
+					["l"] = "open",
+					["h"] = "close_node",
+				},
+			},
+			filesystem = {
+				follow_current_file = {
+					enabled = true,
+				},
+			},
+			default_component_configs = {
+				git_status = {
+					symbols = {
+						-- Change type
+						added = "A", -- or "✚"
+						modified = "M", -- or ""
+						deleted = "D", -- this can only be used in the git_status source
+						renamed = "R", -- this can only be used in the git_status source
+						-- Status type
+						untracked = "U",
+						ignored = "",
+						unstaged = "",
+						staged = "",
+						conflict = "",
 					},
 				},
-				filesystem = {
-					follow_current_file = {
-						enabled = true,
-					},
-				},
-				default_component_configs = {
-					git_status = {
-						symbols = {
-							-- Change type
-							added = "A", -- or "✚"
-							modified = "M", -- or ""
-							deleted = "D", -- this can only be used in the git_status source
-							renamed = "R", -- this can only be used in the git_status source
-							-- Status type
-							untracked = "U",
-							ignored = "",
-							unstaged = "",
-							staged = "",
-							conflict = "",
-						},
-					},
-				},
-			})
-		end,
+			},
+		},
 	},
 }
