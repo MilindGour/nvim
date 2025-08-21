9321798c61e391b52e14e7596f806779b7ae1192
Milind Gour <milind.gour@nagarro.com>
2025-08-21 14:08:38 +0530

    Enhancements

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -2,7 +2,7 @@
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
 	callback = function()
-		vim.hl.on_yank()
+		vim.hl.on_yank({ timeout = 300 })
 	end,
 	group = highlight_group,
 	pattern = "*",
