b6188bc4a6eca68615cc3c79b3e1a93cc33ca91e
Milind Gour <milind.gour@nagarro.com>
2025-11-06 17:25:37 +0530

    Add more Gitsigns keymaps

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -2,6 +2,33 @@ return {
 	{
 		"lewis6991/gitsigns.nvim",
 		event = "BufReadPre",
+		keys = {
+			{
+				"<leader>gH",
+				":Gitsigns preview_hunk<CR>",
+				desc = "Show Hunks in buffer",
+			},
+			{
+				"<leader>gN",
+				":Gitsigns nav_hunk next<CR>",
+				desc = "Goto Next Hunk",
+			},
+			{
+				"<leader>gP",
+				":Gitsigns nav_hunk prev<CR>",
+				desc = "Goto Prev Hunk",
+			},
+			{
+				"<leader>gR",
+				":Gitsigns reset_hunk",
+				desc = "Reset this Hunk",
+			},
+			{
+				"<leader>gS",
+				":Gitsigns stage_hunk",
+				desc = "Stage this Hunk",
+			},
+		},
 		opts = {
 			signs = {
 				add = { text = "+" },
