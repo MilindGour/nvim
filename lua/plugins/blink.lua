865e07df953209be62a91715a59529aab7d61c63
Milind Gour <milind.gour@nagarro.com>
2026-08-01 10:35:51 +0530

    House keeping

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -33,6 +33,11 @@ return {
 
 		-- (Default) Only show the documentation popup when manually triggered
 		completion = {
+			accept = {
+				auto_brackets = {
+					enabled = false,
+				},
+			},
 			documentation = {
 				auto_show = true,
 				auto_show_delay_ms = 500,
